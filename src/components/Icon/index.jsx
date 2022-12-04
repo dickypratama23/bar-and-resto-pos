3b227c9be74ad9171c5777a23f4b34a3805fcc3a
Dicky Pratama <dickypratama4792@gmail.com>
2022-12-04 20:23:47 +0700

    icon

---
+++ src/components/Icon/index.jsx
@@ -0,0 +1,11 @@
+import HomeIcon from './HomeIcon.jsx'
+import FoodIcon from './FoodIcon.jsx'
+import DrinkIcon from './DrinkIcon.jsx'
+import HistoryIcon from './HistoryIcon.jsx'
+import WalletIcon from './WalletIcon.jsx'
+import PromoIcon from './PromoIcon.jsx'
+import SettingIcon from './SettingIcon.jsx'
+import OpenIcon from './OpenIcon.jsx'
+import CloseIcon from './CloseIcon.jsx'
+
+export { HomeIcon, FoodIcon, DrinkIcon, HistoryIcon, WalletIcon, PromoIcon, SettingIcon, OpenIcon, CloseIcon }
