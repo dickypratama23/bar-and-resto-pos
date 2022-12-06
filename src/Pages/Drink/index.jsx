1d96be0e0a223fab486c251dadb0507c09974942
Dicky Pratama <dickypratama4792@gmail.com>
2022-12-06 21:59:10 +0700

    bill only food and drink menu

--- src/Pages/Drink/index.jsx
+++ src/Pages/Drink/index.jsx
@@ -6,6 +6,7 @@ import { useSearchParams } from 'react-router-dom'
 import Search from '../../components/Search'
 import Category from '../../components/Category/index.jsx'
 import DrinkMenuCard from '../../components/DrinkMenuCard/index.jsx'
+import Bill from '../../components/Bill/index.jsx'
 
 const Drink = () => {
   const [searchParams] = useSearchParams()
@@ -23,35 +24,38 @@ const Drink = () => {
   }, [menu])
 
   return (
-    <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
-      <div className="flex justify-between items-center py-10 h-fit">
-        <div className="px-10 text-2xl font-bold">Choose Category</div>
-        <div className="px-10">
-          <Search/>
+    <>
+      <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
+        <div className="flex justify-between items-center py-10 h-fit">
+          <div className="px-10 text-2xl font-bold">Choose Category</div>
+          <div className="px-10">
+            <Search/>
+          </div>
         </div>
-      </div>
-      <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
-        {
-          categories && categories.map((category, index) => {
-            return (
-              <Category key={index} category={category}/>
-            )
-          })
-        }
-      </div>
-      <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu.replaceAll('_', ' ')} Menu</div>
-      <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
-        <div className="grid grid-cols-4 gap-4">
+        <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
           {
-            drinks && drinks.map((drink, index) => {
+            categories && categories.map((category, index) => {
               return (
-                <DrinkMenuCard key={index} drink={drink}/>
+                <Category key={index} category={category}/>
               )
             })
           }
         </div>
+        <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu.replaceAll('_', ' ')} Menu</div>
+        <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
+          <div className="grid grid-cols-4 gap-4">
+            {
+              drinks && drinks.map((drink, index) => {
+                return (
+                  <DrinkMenuCard key={index} drink={drink}/>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
-    </div>
+      <Bill />
+    </>
   )
 }
 
--- src/Pages/Food/index.jsx
+++ src/Pages/Food/index.jsx
@@ -6,6 +6,7 @@ import { useSearchParams } from 'react-router-dom'
 import Search from '../../components/Search'
 import Category from '../../components/Category/index.jsx'
 import FoodMenuCard from '../../components/FoodMenuCard/index.jsx'
+import Bill from '../../components/Bill/index.jsx'
 
 const Food = () => {
   const [searchParams] = useSearchParams()
@@ -23,35 +24,38 @@ const Food = () => {
   }, [menu]);
 
   return (
-    <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
-      <div className="flex justify-between items-center py-10 h-fit">
-        <div className="px-10 text-2xl font-bold">Choose Category</div>
-        <div className="px-10">
-          <Search/>
+    <>
+      <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
+        <div className="flex justify-between items-center py-10 h-fit">
+          <div className="px-10 text-2xl font-bold">Choose Category</div>
+          <div className="px-10">
+            <Search/>
+          </div>
         </div>
-      </div>
-      <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
-        {
-          categories && categories.map((category, index) => {
-            return (
-              <Category key={index} category={category}/>
-            )
-          })
-        }
-      </div>
-      <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu} Menu</div>
-      <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
           {
-            meals && meals.map((meal, index) => {
+            categories && categories.map((category, index) => {
               return (
-                <FoodMenuCard key={index} meal={meal}/>
+                <Category key={index} category={category}/>
               )
             })
           }
         </div>
+        <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu} Menu</div>
+        <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
+          <div className="grid grid-cols-3 gap-4">
+            {
+              meals && meals.map((meal, index) => {
+                return (
+                  <FoodMenuCard key={index} meal={meal}/>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
-    </div>
+      <Bill />
+    </>
   )
 }
 
