b2c62d537c3b5fb4873ee1955709e978032b471c
Dicky Pratama <dickypratama4792@gmail.com>
2022-12-08 14:23:35 +0700

    loading before return response

--- src/pages/Drink/index.jsx
+++ src/pages/Drink/index.jsx
@@ -1,50 +1,72 @@
-import { useFetchDrinkCategoriesQuery, useFilterDrinksQuery } from '../../features/drinks/drinkApiSlice.jsx'
-import { useSearchParams } from 'react-router-dom'
+import {useEffect, useState} from "react";
+import {useFetchDrinkCategoriesQuery, useFilterDrinksQuery} from '../../features/drinks/drinkApiSlice.jsx'
+import {useSearchParams} from 'react-router-dom'
 
 import Search from '../../components/Search'
 import Category from '../../components/Category/index.jsx'
 import DrinkMenuCard from '../../components/DrinkMenuCard/index.jsx'
 import Bill from '../../components/Bill/index.jsx'
+import Loading from "../../components/Loading/index.jsx";
 
 const Drink = () => {
+  const [loading, setLoading] = useState(false);
+
   const [searchParams] = useSearchParams()
   const menu = searchParams.get('menu')
 
-  const { data: categories } = useFetchDrinkCategoriesQuery()
-  const { data: filters } = useFilterDrinksQuery(menu)
+  const {data: categories, isLoading} = useFetchDrinkCategoriesQuery()
+  const {data: filters} = useFilterDrinksQuery(menu)
+
+  useEffect(() => {
+    setLoading(true)
+  }, [menu]);
+
+  useEffect(() => {
+    setLoading(false)
+  }, [filters]);
 
   return (
     <>
-      <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
-        <div className="flex justify-between items-center py-10 h-fit">
-          <div className="px-10 text-2xl font-bold">Choose Category</div>
-          <div className="px-10">
-            <Search/>
-          </div>
-        </div>
-        <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
-          {
-            categories && categories.drinks.map((category, index) => {
-              return (
-                <Category key={index} category={category}/>
-              )
-            })
-          }
-        </div>
-        <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu.replaceAll('_', ' ')} Menu</div>
-        <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
-          <div className="grid grid-cols-4 gap-4">
-            {
-              filters && filters.drinks.map((drink, index) => {
-                return (
-                  <DrinkMenuCard key={index} drink={drink}/>
-                )
-              })
-            }
-          </div>
-        </div>
-      </div>
-      <Bill />
+      {
+        isLoading ? <Loading/> : (
+          <>
+            <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
+              <div className="flex justify-between items-center py-10 h-fit">
+                <div className="px-10 text-2xl font-bold">Choose Category</div>
+                <div className="px-10">
+                  <Search/>
+                </div>
+              </div>
+              <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
+                {
+                  categories && categories.drinks.map((category, index) => {
+                    return (
+                      <Category key={index} category={category}/>
+                    )
+                  })
+                }
+              </div>
+              <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu.replaceAll('_', ' ')} Menu</div>
+              <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
+                {
+                  loading ? <Loading/> : (
+                    <div className="grid grid-cols-4 gap-4">
+                      {
+                        filters && filters.drinks.map((drink, index) => {
+                          return (
+                            <DrinkMenuCard key={index} drink={drink}/>
+                          )
+                        })
+                      }
+                    </div>
+                  )
+                }
+              </div>
+            </div>
+            <Bill/>
+          </>
+        )
+      }
     </>
   )
 }
--- src/pages/Food/index.jsx
+++ src/pages/Food/index.jsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react";
 import {useFetchFoodCategoriesQuery, useFilterFoodsQuery} from "../../features/foods/foodApiSlice.jsx";
 import {useSearchParams} from 'react-router-dom'
 
@@ -5,46 +6,67 @@ import Search from '../../components/Search'
 import Category from '../../components/Category/index.jsx'
 import FoodMenuCard from '../../components/FoodMenuCard/index.jsx'
 import Bill from '../../components/Bill/index.jsx'
+import Loading from "../../components/Loading/index.jsx";
 
 const Food = () => {
+  const [loading, setLoading] = useState(false);
+
   const [searchParams] = useSearchParams()
   const menu = searchParams.get('menu')
 
-  const { data: categories } = useFetchFoodCategoriesQuery()
-  const { data: filters } = useFilterFoodsQuery(menu)
+  const {data: categories, isLoading} = useFetchFoodCategoriesQuery()
+  const {data: filters} = useFilterFoodsQuery(menu)
+
+  useEffect(() => {
+    setLoading(true)
+  }, [menu]);
+
+  useEffect(() => {
+    setLoading(false)
+  }, [filters]);
 
   return (
     <>
-      <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
-        <div className="flex justify-between items-center py-10 h-fit">
-          <div className="px-10 text-2xl font-bold">Choose Category</div>
-          <div className="px-10">
-            <Search/>
-          </div>
-        </div>
-        <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
-          {
-            categories && categories.categories.map((category, index) => {
-              return (
-                <Category key={index} category={category}/>
-              )
-            })
-          }
-        </div>
-        <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu} Menu</div>
-        <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
-          <div className="grid grid-cols-3 gap-4">
-            {
-              filters && filters.meals.map((meal, index) => {
-                return (
-                  <FoodMenuCard key={index} meal={meal}/>
-                )
-              })
-            }
-          </div>
-        </div>
-      </div>
-      <Bill/>
+      {
+        isLoading ? <Loading/> : (
+          <>
+            <div className="w-9/12 bg-[#FEFBF6] grid grid-flow-row auto-rows-max">
+              <div className="flex justify-between items-center py-10 h-fit">
+                <div className="px-10 text-2xl font-bold">Choose Category</div>
+                <div className="px-10">
+                  <Search/>
+                </div>
+              </div>
+              <div className="px-10 flex gap-2 h-fit scrollbar-hide overflow-x-auto">
+                {
+                  categories && categories.categories.map((category, index) => {
+                    return (
+                      <Category key={index} category={category}/>
+                    )
+                  })
+                }
+              </div>
+              <div className="px-10 py-5 text-2xl font-bold capitalize h-fit">{menu} Menu</div>
+              <div className="px-10 scrollbar-hide overflow-y-auto h-[690px]">
+                {
+                  loading ? <Loading/> : (
+                    <div className="grid grid-cols-3 gap-4">
+                      {
+                        filters && filters.meals.map((meal, index) => {
+                          return (
+                            <FoodMenuCard key={index} meal={meal}/>
+                          )
+                        })
+                      }
+                    </div>
+                  )
+                }
+              </div>
+            </div>
+            <Bill/>
+          </>
+        )
+      }
     </>
   )
 }
